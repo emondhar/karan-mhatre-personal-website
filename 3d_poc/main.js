d7dcf06d9e5da83a3066289e40b676bfea2e7f02
Karan Mhatre <me@karanmhatre.com>
2020-05-21 16:27:58 +0530

    3d_poc added

---
+++ 3d_poc/main.js
@@ -0,0 +1,46 @@
+$(function() {
+  $('#imgModel').attr('draggable', false);
+
+  var imgContainer = $('#imgContainer').get(0);
+  var mc = new Hammer(imgContainer);
+
+  mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+
+  var count = 15;
+
+  mc.on("panleft panright", function(ev) {
+
+    var imgModel = $('#imgModel'),
+        currentImg = parseInt(imgModel.attr('src').split('/')[1].split('.')[0]);
+
+     speed = 0.3;
+
+    if(ev.type == 'panleft') {
+      count = count + speed
+      newImg = Math.floor(count);
+    }
+    else if(ev.type == 'panright') {
+      count = count - speed
+      newImg = Math.floor(count);
+    }
+
+    // console.log(ev.type);
+    console.log(count);
+
+    if(count > 35)
+      count = 35;
+
+    if(count <= 0) 
+      count = 1;
+
+    if(newImg > 35)
+      newImg = 35;
+
+    if(newImg <= 0) 
+      newImg = 1;
+
+     imgModel.attr('src', 'img3d/' + newImg + '.jpg');
+
+  });
+
+});
